c87e3ed8311b77bbe5a9fccbf13b1d664db54f2e
Juan Briones <jbrionesgmm@gmail.com>
2023-11-16 15:13:13 -0700

    fixed root comp

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />,
+        element: <PlayGames />,
       },
       {
         path: "/login",
